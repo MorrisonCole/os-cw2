d3121a7c95c3dc129014e7c7723d029ed1729607
Morrison Cole <admin@morrisoncole.co.uk>
2014-01-04 18:18:07 +0000

    Minor formatting for readability

--- dos_scandisk.c
+++ dos_scandisk.c
@@ -17,9 +17,9 @@
 
 void print_indent(int indent)
 {
-  int i;
-  for (i = 0; i < indent; i++)
-    printf(" ");
+    int i;
+    for (i = 0; i < indent; i++)
+        printf(" ");
 }
 
 void follow_dir(uint16_t cluster, int indent,
@@ -28,9 +28,9 @@ void follow_dir(uint16_t cluster, int indent,
     struct direntry *dirent;
     int d, i;
     dirent = (struct direntry*) cluster_to_addr(cluster, image_buf, bpb);
+
     while (1) {
-        for (d = 0; d < bpb->bpbBytesPerSec * bpb->bpbSecPerClust;
-             d += sizeof(struct direntry)) {
+        for (d = 0; d < bpb->bpbBytesPerSec * bpb->bpbSecPerClust; d += sizeof(struct direntry)) {
             char name[9];
             char extension[4];
             uint32_t size;
@@ -39,68 +39,70 @@ void follow_dir(uint16_t cluster, int indent,
             extension[3] = ' ';
             memcpy(name, &(dirent->deName[0]), 8);
             memcpy(extension, dirent->deExtension, 3);
+
             if (name[0] == SLOT_EMPTY)
-            return;
+                return;
 
             /* skip over deleted entries */
             if (((uint8_t)name[0]) == SLOT_DELETED)
-            continue;
+                continue;
 
             /* names are space padded - remove the spaces */
             for (i = 8; i > 0; i--) {
-            if (name[i] == ' ')
-                name[i] = '\0';
-            else
-                break;
+                if (name[i] == ' ')
+                    name[i] = '\0';
+                else
+                    break;
             }
 
             /* remove the spaces from extensions */
             for (i = 3; i > 0; i--) {
-            if (extension[i] == ' ')
-                extension[i] = '\0';
-            else
-                break;
+                if (extension[i] == ' ')
+                    extension[i] = '\0';
+                else
+                    break;
             }
 
             /* don't print "." or ".." directories */
-            if (strcmp(name, ".")==0) {
-            dirent++;
-            continue;
+            if (strcmp(name, ".") == 0) {
+                dirent++;
+                continue;
             }
-            if (strcmp(name, "..")==0) {
-            dirent++;
-            continue;
+            if (strcmp(name, "..") == 0) {
+                dirent++;
+                continue;
             }
 
             if ((dirent->deAttributes & ATTR_VOLUME) != 0) {
-            printf("Volume: %s\n", name);
+                printf("Volume: %s\n", name);
             } else if ((dirent->deAttributes & ATTR_DIRECTORY) != 0) {
                 print_indent(indent);
-            printf("%s (directory)\n", name);
-            file_cluster = getushort(dirent->deStartCluster);
-            follow_dir(file_cluster, indent+2, image_buf, bpb);
+                printf("%s (directory)\n", name);
+                file_cluster = getushort(dirent->deStartCluster);
+                follow_dir(file_cluster, indent+2, image_buf, bpb);
             } else {
-            size = getulong(dirent->deFileSize);
+                size = getulong(dirent->deFileSize);
                 print_indent(indent);
-            printf("%s.%s (%u bytes)\n",
-                   name, extension, size);
+                printf("%s.%s (%u bytes)\n", name, extension, size);
             }
+
             dirent++;
         }
+
+        /* We've reached the end of the cluster for this directory. Where's the next cluster? */
         if (cluster == 0) {
             // root dir is special
             dirent++;
         } else {
             cluster = get_fat_entry(cluster, image_buf, bpb);
-            dirent = (struct direntry*)cluster_to_addr(cluster,
-                                   image_buf, bpb);
+            dirent = (struct direntry*) cluster_to_addr(cluster, image_buf, bpb);
         }
     }
 }
 
 void usage()
 {
-    fprintf(stderr, "Usage: dos_ls <imagename>\n");
+    fprintf(stderr, "Usage: dos_scandisk <imagename>\n");
     exit(1);
 }
 
@@ -109,7 +111,7 @@ int main(int argc, char** argv)
     uint8_t *image_buf;
     int fd;
     struct bpb33* bpb;
-    if (argc < 2 || argc > 2) {
+    if (argc != 2) {
         usage();
     }
 
